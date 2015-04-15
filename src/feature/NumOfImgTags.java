58ebbe15de538b478273bb47164fdbbf28926213
Jiasong Sun <iamjasonsun@gmail.com>
2015-04-15 14:34:19 -0400

    add mian method and test file

--- src/feature/NumOfImgTags.java
+++ src/feature/NumOfImgTags.java
@@ -1,5 +1,9 @@
 package feature;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class NumOfImgTags implements Feature {
@@ -8,6 +12,12 @@ public class NumOfImgTags implements Feature {
     public int getFeatureValue(Document document) {
         // TODO Auto-generated method stub
         return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Document document = Jsoup.parse(new File("testFile.html"), "UTF-8");
+        Feature numOfImgTangs = new NumOfImgTags();
+        System.out.println(numOfImgTangs.getFeatureValue(document));
     }
 
 }
